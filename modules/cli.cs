76dabcf895bfc67c6e174ac30047511281558a9c
Abjust <2533898108@qq.com>
2022-07-02 18:50:56 +0800

    b1.0.3

---
+++ modules/cli.cs
@@ -0,0 +1,45 @@
+﻿using Mirai.Net.Data.Messages;
+using Mirai.Net.Data.Messages.Receivers;
+using Mirai.Net.Sessions.Http.Managers;
+using Mirai.Net.Utils.Scaffolds;
+
+namespace Mirai.Net_2kBot.Modules
+{
+    public static class CLI
+    {
+        public static async void Execute(MessageReceiverBase @base)
+        {
+            if (@base is FriendMessageReceiver receiver)
+            {
+                if (receiver.MessageChain.GetPlainMessage().ToLower().StartsWith("/send") == true)
+                {
+                    string[] text = receiver.MessageChain.GetPlainMessage().ToString().Split(" ");
+                    if (text.Length >= 3)
+                    {
+                        string results = "";
+                        for (int i = 2; i < text.Length; i++)
+                        {
+                            if (i == 2)
+                            {
+                                results = text[i];
+                            }
+                            else
+                            {
+                                results = results + " " + text[i];
+                            }
+                        }
+                        try
+                        {
+                            await MessageManager.SendGroupMessageAsync(text[1], results);
+                        }
+                        catch { }
+                    }
+                    else
+                    {
+                        await receiver.SendMessageAsync("参数缺少");
+                    }
+                }
+            }
+        }
+    }
+}
